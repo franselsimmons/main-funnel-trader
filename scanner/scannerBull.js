2570ac0c85cede508a2fb796499e060d2c937c36
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-12 09:48:34 +0200

    Update scannerBull.js

--- scanner/scannerBull.js
+++ scanner/scannerBull.js
@@ -1,13 +1,17 @@
+import { fetchUniverse } from "./universeFetcher.js"
+import { runScanner } from "./scannerCore.js"
 import { BULL_CONFIG } from "../config/bull.js"
 import { SHARED_CONFIG } from "../config/shared.js"
-import { runScanner } from "./scannerCore.js"
 
-export async function runBullScanner(marketData, btcData) {
+export async function runBullScanner(btcData) {
+
+  const universe = await fetchUniverse()
+
   return await runScanner({
     mode: "bull",
     config: BULL_CONFIG,
     sharedConfig: SHARED_CONFIG,
-    marketData,
+    marketData: universe,
     btcData
   })
 }
