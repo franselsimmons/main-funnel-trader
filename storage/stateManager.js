07b2519c8429d81146203c49422b2280d2a739e7
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-12 09:09:10 +0200

    Create stateManager.js

---
+++ storage/stateManager.js
@@ -0,0 +1,15 @@
+export function scannerKey(mode) {
+  return `main:v6:${mode}:scanner`
+}
+
+export function funnelKey(mode) {
+  return `main:v6:${mode}:funnel`
+}
+
+export function portfolioKey(mode) {
+  return `main:v6:${mode}:portfolio`
+}
+
+export function analyticsKey(mode) {
+  return `main:v6:${mode}:analytics`
+}
