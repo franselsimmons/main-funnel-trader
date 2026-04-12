66d226a0099630ec72c1edbff9add40b9e80d549
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-12 10:50:18 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,15 @@
 import Link from "next/link"
-import { useRouter } from "next/router"
 
 export default function Navbar() {
-  const router = useRouter()
-
-  const linkStyle = (path) => ({
-    marginLeft: 20,
-    textDecoration: "none",
-    color: router.pathname === path ? "#fff" : "#888",
-    fontWeight: 500
-  })
-
   return (
-    <div className="navbar">
-      <div><strong>MAIN V6</strong></div>
-      <div className="nav-links">
-        <Link href="/" style={linkStyle("/")}>Home</Link>
-        <Link href="/bull" style={linkStyle("/bull")}>Bull</Link>
-        <Link href="/bear" style={linkStyle("/bear")}>Bear</Link>
-        <Link href="/analyse" style={linkStyle("/analyse")}>Analyse</Link>
-      </div>
+    <div style={{
+      display: "flex",
+      gap: 20,
+      padding: 20,
+      borderBottom: "1px solid #222"
+    }}>
+      <Link href="/bull">Bull</Link>
+      <Link href="/bear">Bear</Link>
     </div>
   )
 }
