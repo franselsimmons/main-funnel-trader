6621df95989e42460e0abb12628379fddf847497
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-13 21:04:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    document.body.classList.add("fade-in");
+  }, []);
+
   return <Component {...pageProps} />;
 }
