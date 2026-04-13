f674d39c602aa89f3557672764c340e100d5abb2
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-13 17:18:23 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,31 +1,11 @@
 import Navbar from "./Navbar"
 
-export default function Layout({ title, lastScan, children }) {
-
-  const formatTime = (timestamp) => {
-    if (!timestamp) return "Never"
-    return new Date(timestamp).toLocaleString()
-  }
-
+export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-
-      <div className="page">
-        <div className="page-inner">
-
-          <div className="page-header">
-            <h1>{title}</h1>
-            {lastScan && (
-              <div className="scan-time">
-                Last Scan: {formatTime(lastScan)}
-              </div>
-            )}
-          </div>
-
-          {children}
-
-        </div>
+      <div className="container">
+        {children}
       </div>
     </>
   )
