731797e525710716cd1afe5ca256678d611fcc0a
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-13 19:47:03 +0200

    Delete api directory

--- api/cron/analyse.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const base = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_BASE_URL
-
-  await fetch(`${base}/api/analyse`)
-
-  res.json({ ok: true, job: "analyse" })
-}
--- api/state/bearPortfolio.js
+++
@@ -1,6 +0,0 @@
-import { getKey } from "../../storage/kv.js"
-
-export default async function handler(req, res) {
-  const data = await getKey("portfolio:bear") || []
-  res.json(data)
-}
