14327762cdf33e07c1a496ea2d683a770a4d8602
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-15 20:26:30 +0200

    Update public-latest.js

--- api/public-latest.js
+++ api/public-latest.js
@@ -1,5 +1,5 @@
 import scanner from "./scanner.js";
 
-export default async function handler(req,res){
-  return scanner(req,res);
+export default async function handler(req, res) {
+  return scanner(req, res);
 }
