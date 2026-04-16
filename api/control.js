aea3c5d849857edcbef773bde7eac9f56ebfabad
franselsimmons <lrsdaktechniek@hotmail.com>
2026-04-16 06:36:42 +0200

    Update control.js

--- api/control.js
+++ api/control.js
@@ -1,26 +1,9 @@
-import { getStats } from "../lib/learning.js";
+let MODE = "bull";
 
-let SYSTEM_ON = true;
-
-export function isSystemOn(){
-  return SYSTEM_ON;
-}
-
-export default function handler(req,res){
-
-  const stats = getStats();
-
-  // Auto shutdown
-  if(stats.total > 20 && stats.winrate < 40){
-    SYSTEM_ON = false;
+export default function handler(req, res) {
+  if (req.query.mode) {
+    MODE = req.query.mode;
   }
 
-  // Manual override
-  if(req.query.force === "on") SYSTEM_ON = true;
-  if(req.query.force === "off") SYSTEM_ON = false;
-
-  res.json({
-    system: SYSTEM_ON ? "ON" : "OFF",
-    winrate: stats.winrate
-  });
+  res.json({ mode: MODE });
 }
